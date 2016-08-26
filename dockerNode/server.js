3b75733c90b5190e8a472939d275aa0bac770012
Jason Ramirez <jp51371@iastate.edu>
2016-08-26 15:47:01 -0500

    Creating small chat site

--- dockerNode/server.js
+++ dockerNode/server.js
@@ -1,11 +1,15 @@
-var express = require('express');
+'use strict';
 
-var PORT = 8080;
+const express = require('express');
 
-var app = express();
-app.get('/', function(req, res){
-  res.send('Hello World');
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
 });
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log('Running on http://localhost:' + PORT)
